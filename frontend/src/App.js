0e5cdba532578c1ec72b4b82d59b8235a15f1491
figo5530 <figooooo0221@gmail.com>
2021-07-04 16:37:08 +0800

    adds shopinput component and finishes basic features.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import NavBar from './NavBar'
-import React from 'react';
+import React from 'react'
+import ShopInput from './shops/ShopInput';
 
 function App () {
     return (
-      <>
-        <NavBar/>
-      </>
+      <div>
+        <NavBar />
+        <ShopInput />
+      </div>
     )
 }
 export default App;
---
+++ frontend/src/shops/ShopInput.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+class ShopInput extends Component {
+    state = {
+        text: ''
+    }
+
+    handleChange = e => {
+        this.setState({
+          text: e.target.value
+        })
+    }
+
+    hanldeSubmit = e => {
+        e.preventDefault()
+        console.log(this.state.text)
+        //to be finished
+        this.setState({
+            text: ''
+        })
+    }
+
+    render() {
+        return(
+        <div>
+            <label>search shop</label>
+            <form onSubmit={this.hanldeSubmit}>
+                <input type='text' onChange={this.handleChange} value={this.state.text}/>
+                <input type='submit'/>
+            </form>
+        </div>
+        )
+    }
+}
+
+export default ShopInput
