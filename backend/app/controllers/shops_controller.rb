e929c804b1b69f3b05e14c6df4780fe53267835f
figo5530 <figooooo0221@gmail.com>
2021-07-05 18:09:53 +0800

    adds show action

--- backend/app/controllers/shops_controller.rb
+++ backend/app/controllers/shops_controller.rb
@@ -2,4 +2,9 @@ class ShopsController < ApplicationController
     def index
         render json: Shop.all
     end
+
+    def show
+        shop = Shop.find_by(id: params[:id])
+        render json: shop
+    end
 end
