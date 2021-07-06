aef3600b3ee92d7a989c14d9b86d55a9cfaca7c9
figo5530 <figooooo0221@gmail.com>
2021-07-06 22:44:15 +0800

    finds a way to import images dynamically, still testing.

--- frontend/src/shops/Shop.js
+++ frontend/src/shops/Shop.js
@@ -2,13 +2,14 @@ import React, {Component} from 'react'
 
 export default class Shop extends Component {
         render() {
-            const {name, address, city, state, postal_code, stars, categories} = this.props.shop
+            const {id, name, address, city, state, postal_code, stars, categories} = this.props.shop
+            // const image = require("../img/" + id + ".jpg")
             return(
-                <div>
+                <div className="section-top-border">
 						<h3 class="mb-30 title_color">{name}</h3>
 						<div class="row">
 							<div class="col-md-3">
-								Image
+                                {/* <img alt={name} src={image} width="200" height="200"/> */}
 							</div>
 							<div class="col-md-9 mt-sm-20 left-align-p">
 								<p>{address}, {city}, {state}, {postal_code}</p>
