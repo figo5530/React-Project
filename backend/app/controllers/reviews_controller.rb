b07c4a3ef932d2fa7dc78a53896a7db2a3db0045
figo5530 <figooooo0221@gmail.com>
2021-07-08 18:03:48 +0800

    adds create action in review controller.

--- backend/app/controllers/reviews_controller.rb
+++ backend/app/controllers/reviews_controller.rb
@@ -1,2 +1,11 @@
-class ShopsController < ApplicationController
+class ReviewsController < ApplicationController
+    def create
+        review = Review.create(review_params)
+        render json: review
+    end
+
+    private
+    def review_params
+        params.require(:review).permit(:content, :shop_id)
+    end
 end
