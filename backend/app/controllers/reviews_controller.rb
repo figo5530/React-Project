7945e5a2e3960ab6219b656d6d176a0bc41e4bf9
figo5530 <figooooo0221@gmail.com>
2021-07-08 20:57:53 +0800

    adds destroy action

--- backend/app/controllers/reviews_controller.rb
+++ backend/app/controllers/reviews_controller.rb
@@ -4,6 +4,12 @@ class ReviewsController < ApplicationController
         render json: review
     end
 
+    def destroy
+        review = Review.find_by(id: params[:id])
+        review.destroy
+        render json: {message: "Success!"}
+    end
+    
     private
     def review_params
         params.require(:review).permit(:content, :shop_id)
