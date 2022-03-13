e9d78575abdd9c61121252ad0c6ae317104e6aec
figo5530 <figooooo0221@gmail.com>
2022-03-13 16:49:00 -0400

    clears homePage, deletes navBar, recreates AppBar in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,67 @@
 import './style/style.css'
 import './style/bootstrap.css'
-import NavBar from './NavBar'
 import City from './City'
-import React from 'react'
 import ShopsContainer from './shops/ShopsContainer';
-import {Route, Switch} from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import HomePage from './homepage/HomePage'
 import CityContainer from './CityContainer'
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import React from 'react'
+import { useHistory } from 'react-router-dom';
 
+function App() {
+  const history = useHistory()
+  const pages = ["Home", "Shops", "City"]
+  const handleClick = (page) => {
+    if(page === 'Home') {
+      history.push('/')
+    }else {
+      history.push(`/${page}`)
+    }
+  }
+  return (
+    <>
+      <AppBar position="static">
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
+            >
+              LOGO
+            </Typography>
+            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+              {pages.map((page) => (
+                <Button
+                  key={page}
+                  onClick={() => handleClick(page)}
+                  sx={{ my: 2, color: 'white', display: 'block' }}
+                >
+                  {page}
+                </Button>
+              ))}
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
 
-function App () {
-    return (
-      <>
-      <header className="header_area">
-        <div className="container">
-          <nav className="navbar navbar-expand-lg navbar-light"><NavBar /></nav>
-        </div>
-      </header>
-    
       <Switch>
         <Route path='/shops' render={routerProps => <ShopsContainer {...routerProps} />} />
-        <Route exact path='/' render={routerProps => <HomePage {...routerProps}/>} />
-        <Route path='/city' render={routerProps => <City {...routerProps}/>} />
-        <Route path='/boston/shops' render={() => <CityContainer city="Boston"/>} />
-        <Route path='/newyork/shops' render={() => <CityContainer city="New York"/>} />
-        <Route path='/sanfrancisco/shops' render={() => <CityContainer city="San Francisco"/>} />
-        <Route path='/seattle/shops' render={() => <CityContainer city="Seattle"/>} />
+        <Route exact path='/' render={routerProps => <HomePage {...routerProps} />} />
+        <Route path='/city' render={routerProps => <City {...routerProps} />} />
+        <Route path='/boston/shops' render={() => <CityContainer city="Boston" />} />
+        <Route path='/newyork/shops' render={() => <CityContainer city="New York" />} />
+        <Route path='/sanfrancisco/shops' render={() => <CityContainer city="San Francisco" />} />
+        <Route path='/seattle/shops' render={() => <CityContainer city="Seattle" />} />
       </Switch>
-      </>  
-    )
+    </>
+  )
 }
 export default App;
--- frontend/src/homepage/HomePage.js
+++ frontend/src/homepage/HomePage.js
@@ -1,27 +1,15 @@
 import React, {Component} from "react"
-import './homepageCSS.css'
+// import './homepageCSS.css'
 class HomePage extends Component {
 
-    hanldeSubmit = e => {
-        e.preventDefault()
-        this.props.history.push("/shops")
-    }
+    // hanldeSubmit = e => {
+    //     e.preventDefault()
+    //     this.props.history.push("/shops")
+    // }
     render() {
         return(
-            <div id="cover">
-                <form id="hp-form" onSubmit={this.hanldeSubmit}>
-                    <div className="tb">
-                        <div className="td">
-                            <input id="hp-input" type='text' placeholder="Check it!"></input>
-                        </div>
-                        <div className="td" id="s-cover">
-                            <button id="hp-bt" type="submit">
-                                <div id="s-circle"></div>
-                                <span></span>
-                            </button>
-                        </div>
-                    </div>
-                </form>
+            <div>
+                Home
             </div>
         )
     }
